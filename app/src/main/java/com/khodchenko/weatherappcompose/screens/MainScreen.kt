ea1cb5931be915278506d276c5bd3718aad5107f
khodchenko <khodch3nk0@gmail.com>
2023-11-25 18:29:34 +0100

    Refactor

--- app/src/main/java/com/khodchenko/weatherappcompose/screens/MainScreen.kt
+++ app/src/main/java/com/khodchenko/weatherappcompose/screens/MainScreen.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -27,10 +24,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -41,6 +38,8 @@ import com.google.accompanist.pager.rememberPagerState
 import com.khodchenko.weatherappcompose.R
 import com.khodchenko.weatherappcompose.data.WeatherModel
 import kotlinx.coroutines.launch
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 @Composable
@@ -49,7 +48,6 @@ fun MainCard(currentDay: MutableState<WeatherModel>) {
         modifier = Modifier
             .padding(6.dp)
     ) {
-
         Card(
             modifier = Modifier.fillMaxWidth(),
             colors = CardDefaults.cardColors(
@@ -79,66 +77,69 @@ fun MainCard(currentDay: MutableState<WeatherModel>) {
                         model = "https:${currentDay.value.icon}",
                         contentDescription = "Weather icon",
                         modifier = Modifier
+                            .padding(
+                                top = 8.dp,
+                                end = 8.dp
+                            )
                             .size(35.dp)
-                            .padding(top = 4.dp, end = 8.dp),
                     )
-
                 }
-
                 Text(
                     text = currentDay.value.city,
                     style = TextStyle(fontSize = 24.sp)
                 )
-
                 Text(
-                    text = currentDay.value.currentTemp.toFloat().toInt().toString() + "°C",
-                    style = TextStyle(fontSize = 64.sp)
+                    text = if(currentDay.value.currentTemp.isNotEmpty())
+                        currentDay.value.currentTemp.toFloat().toInt().toString() + "ºC"
+                    else currentDay.value.maxTemp.toFloat().toInt().toString() +
+                            "ºC/${currentDay.value.minTemp.toFloat().toInt()}ºC"
+                    ,
+                    style = TextStyle(fontSize = 65.sp)
                 )
-
                 Text(
                     text = currentDay.value.condition,
                     style = TextStyle(fontSize = 16.sp)
                 )
-
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    IconButton(onClick = {
+                    IconButton(
+                        onClick = {
 
-                    }) {
+                        }
+                    ) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_search),
-                            contentDescription = "image"
+                            contentDescription = "im3",
+                            tint = Color.White
                         )
                     }
-
                     Text(
-                        text = "${
-                            currentDay.value.minTemp.toFloat().toInt()
-                        }°C...${currentDay.value.maxTemp.toFloat().toInt()}°C",
+                        text = "${currentDay.value
+                            .maxTemp.toFloat().toInt()}ºC/${currentDay
+                            .value.minTemp.toFloat().toInt()}ºC",
                         style = TextStyle(fontSize = 16.sp)
                     )
+                    IconButton(
+                        onClick = {
 
-                    IconButton(onClick = {
-
-                    }) {
+                        }
+                    ) {
                         Icon(
                             painter = painterResource(id = R.drawable.ic_sync),
-                            contentDescription = "image"
+                            contentDescription = "im4"
                         )
                     }
                 }
             }
-
         }
-
     }
 }
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
-fun TabLayout(daysList: MutableState<List<WeatherModel>>) {
+fun TabLayout(daysList: MutableState<List<WeatherModel>>, currentDay: MutableState<WeatherModel>) {
     val tabList = listOf("HOURS", "DAYS")
     val pagerState = rememberPagerState()
     val tabIndex = pagerState.currentPage
@@ -155,21 +156,22 @@ fun TabLayout(daysList: MutableState<List<WeatherModel>>) {
                     Modifier.pagerTabIndicatorOffset(
                         pagerState = pagerState,
                         tabPositions = pos
-                    )
+                    ),
+
                 )
-            },
-            modifier = Modifier.background(color = MaterialTheme.colorScheme.surfaceVariant)
+            }
         ) {
             tabList.forEachIndexed { index, text ->
                 Tab(
-                    modifier = Modifier.background(color = MaterialTheme.colorScheme.surfaceVariant), //todo change color
                     selected = false,
                     onClick = {
                         coroutineScope.launch {
                             pagerState.animateScrollToPage(index)
                         }
                     },
-                    text = { Text(text = text) })
+                    text = { Text(text = text) },
+                    modifier = Modifier.background(MaterialTheme.colorScheme.surfaceVariant))
+
             }
         }
         HorizontalPager(
@@ -177,16 +179,34 @@ fun TabLayout(daysList: MutableState<List<WeatherModel>>) {
             state = pagerState,
             modifier = Modifier.weight(1.0f)
         ) { index ->
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                itemsIndexed(
-                    daysList.value
-                ) { _, item ->
-                    ListItem(item)
-                }
-
-
+            val list = when(index){
+                0 -> getWeatherByHours(currentDay.value.hours)
+                1 -> daysList.value
+                else -> daysList.value
             }
-
+            MainList(list, currentDay)
         }
     }
+}
+
+private fun getWeatherByHours(hours: String): List<WeatherModel>{
+    if (hours.isEmpty()) return listOf()
+    val hoursArray = JSONArray(hours)
+    val list = ArrayList<WeatherModel>()
+    for (i in 0 until hoursArray.length()){
+        val item = hoursArray[i] as JSONObject
+        list.add(
+            WeatherModel(
+                "",
+                item.getString("time"),
+                item.getString("temp_c").toFloat().toInt().toString() + "ºC",
+                item.getJSONObject("condition").getString("text"),
+                item.getJSONObject("condition").getString("icon"),
+                "",
+                "",
+                ""
+            )
+        )
+    }
+    return list
 }
